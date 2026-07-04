0c402b0dac0787836f2c9121c3ae1b6f31a394e2
CurtisD4q <31763705+CurtisD4q@users.noreply.github.com>
2026-07-04 10:09:43 +0100

    Create waybar-effects.sh for Waybar effects
    Add script to reapply Waybar layer effects on output change

---
+++ sway/waybar-effects.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# Reapply Waybar layer effects whenever outputs change
+swaymsg 'layer_effects "waybar" corner_radius 3'
+# swaymsg 'layer_effects "waybar" blur enable; shadows enable; shadow_blur_radius 20'
