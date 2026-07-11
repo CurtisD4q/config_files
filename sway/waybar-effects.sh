4eace16df4857c85da8e0866d9a728c3049ef29a
CurtisD4q <31763705+CurtisD4q@users.noreply.github.com>
2026-07-11 04:10:51 +0100

    Delete sway/waybar-effects.sh

--- sway/waybar-effects.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# Reapply Waybar layer effects whenever outputs change
-swaymsg 'layer_effects "waybar" corner_radius 3'
-swaymsg 'layer_effects "waybar" blur enable; shadows enable; shadow_blur_radius 20'
